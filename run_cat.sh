f6682d6ca0d4342ae2175d6bf8933c3fbd502b18
Verse Uni <akamosko@gmail.com>
2022-05-15 14:25:25 +0300

    check for freeze.
    Didn't find reason yet, but sometimes app freeze, so we check this case by app.log modification date.

--- run_cat.sh
+++ run_cat.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/bash
-if pgrep -f "cat.py" &>/dev/null; then
-    echo "it is already running"
+cur_time=`date +%s`
+last_mod=`stat -c "%Y" log/app.log`
+c_pid=`pgrep -f "cat.py"`
+diff_sec=$(($cur_time-$last_mod))
+#if process is running
+if [ ! -z "$c_pid" ]; then
+#if there is no log update for 5 min - kill it and restart.
+  if [ $diff_sec -gt 300 ]; then
+    kill $c_pid
+    python3 cat.py
+  else 
     exit
-else
+  fi  
+else 
     python3 cat.py
 fi
